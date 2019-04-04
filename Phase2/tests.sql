b6847b5c45f9da34f72c81a658b2ba285ba8b24a
Ti-ruo Yan <yantiruo@gmail.com>
2019-04-04 15:47:04 -0400

    working on 1.3

--- Phase2/tests.sql
+++ Phase2/tests.sql
@@ -10,3 +10,15 @@ delete from temp1 where count < (select count(route_id) from routes);
 
 --select station_id,count(station_id) into temp2 from routes_and_station_status group by station_id;
 select count(route_id) from routes;
+--
+
+select route_id into z from train_schedule group by route_id;
+select * into z1 from routes_and_station_status;
+delete from z1 where route_id not in (select route_id from z);
+delete from z1 where station_status = true;
+select station_id, count(station_id) into z2 from z1 group by station_id;
+delete from z2 where count < (select count(route_id) from z1);
+
+--
+select route_id, rail_id, routes_and_station_status.station_id into z from routes_and_station_status left join rail_stations on routes_and_station_status.station_id = rail_stations.station_id;
+select route_id, rail_id, count(*) from z 
